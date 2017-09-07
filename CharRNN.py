21fe881a0856055dd24f8e1d83236ff1b1de3170
Tsubasa Ogawa <hurikake@gmail.com>
2017-09-07 23:34:17 +0900

    escape deprecated expression: volatile train

--- CharRNN.py
+++ CharRNN.py
@@ -19,16 +19,17 @@ class CharRNN(Chain):
         #     param[:] = np.random.uniform(-0.08, 0.08, param.shape)
 
     def forward_one_step(self, x_data, y_data, state, train=True, dropout_ratio=0.5):
-        x = Variable(x_data, volatile=not train)
-        t = Variable(y_data, volatile=not train)
+        with chainer.using_config('enable_backprop', True):
+            x = Variable(x_data)
+            t = Variable(y_data)
 
-        h0      = self.embed(x)
-        h1_in   = self.l1_x(F.dropout(h0, ratio=dropout_ratio, train=train)) + self.l1_h(state['h1'])
-        c1, h1  = F.lstm(state['c1'], h1_in)
-        h2_in   = self.l2_x(F.dropout(h1, ratio=dropout_ratio, train=train)) + self.l2_h(state['h2'])
-        c2, h2  = F.lstm(state['c2'], h2_in)
-        y       = self.l3(F.dropout(h2, ratio=dropout_ratio, train=train))
-        state   = {'c1': c1, 'h1': h1, 'c2': c2, 'h2': h2}
+            h0      = self.embed(x)
+            h1_in   = self.l1_x(F.dropout(h0, ratio=dropout_ratio)) + self.l1_h(state['h1'])
+            c1, h1  = F.lstm(state['c1'], h1_in)
+            h2_in   = self.l2_x(F.dropout(h1, ratio=dropout_ratio)) + self.l2_h(state['h2'])
+            c2, h2  = F.lstm(state['c2'], h2_in)
+            y       = self.l3(F.dropout(h2, ratio=dropout_ratio))
+            state   = {'c1': c1, 'h1': h1, 'c2': c2, 'h2': h2}
 
         if train:
             return state, F.softmax_cross_entropy(y, t)
@@ -36,6 +37,6 @@ class CharRNN(Chain):
             return state, F.softmax(y)
 
 def make_initial_state(n_units, batchsize=50, train=True):
-    return {name: Variable(np.zeros((batchsize, n_units), dtype=np.float32),
-            volatile=not train)
+    with chainer.using_config('enable_backprop', True):
+        return {name: Variable(np.zeros((batchsize, n_units), dtype=np.float32))
             for name in ('c1', 'h1', 'c2', 'h2')}

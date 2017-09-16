a18dc0d8d42a91ebdede7fcaeb7a934e16092e67
Tsubasa Ogawa <hurikake@gmail.com>
2017-09-16 16:32:51 +0900

    modify CharRNN() for Chainer 2 + Python 3

--- CharRNN.py
+++ CharRNN.py
@@ -23,15 +23,15 @@ class CharRNN(Chain):
             x = Variable(x_data)
             t = Variable(y_data)
 
-            h0      = self.embed(x)
-            h1_in   = self.l1_x(F.dropout(h0, ratio=dropout_ratio)) + self.l1_h(state['h1'])
-            c1, h1  = F.lstm(state['c1'], h1_in)
-            h2_in   = self.l2_x(F.dropout(h1, ratio=dropout_ratio)) + self.l2_h(state['h2'])
-            c2, h2  = F.lstm(state['c2'], h2_in)
-            y       = self.l3(F.dropout(h2, ratio=dropout_ratio))
-            state   = {'c1': c1, 'h1': h1, 'c2': c2, 'h2': h2}
+        h0      = self.embed(x)
+        h1_in   = self.l1_x(F.dropout(h0, ratio=dropout_ratio)) + self.l1_h(state['h1'])
+        c1, h1  = F.lstm(state['c1'], h1_in)
+        h2_in   = self.l2_x(F.dropout(h1, ratio=dropout_ratio)) + self.l2_h(state['h2'])
+        c2, h2  = F.lstm(state['c2'], h2_in)
+        y       = self.l3(F.dropout(h2, ratio=dropout_ratio))
+        state   = {'c1': c1, 'h1': h1, 'c2': c2, 'h2': h2}
 
-        if train:
+        if chainer.config.train:
             return state, F.softmax_cross_entropy(y, t)
         else:
             return state, F.softmax(y)
